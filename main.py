51b365081020f5ab653421bbb992405542c61e9d
Zephyr Worthington <zephyr@Zephyrs-MacBook-Pro.local>
2021-02-12 12:46:57 -0800

    Create generate_password function. Add generate_password command to Generate Password button

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
 
 DEFAULT_EMAIL = "zephyrworthington@gmail.com"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def generate_password():
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_nums = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_nums
+    shuffle(password_list)
+
+    password = ''.join(password_list)
+    password_input.delete(0, 'end')
+    password_input.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def reset_form():
@@ -72,7 +93,7 @@ password_input.grid(row=3, column=1)
 
 # Buttons
 # Generate Password Button
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 
 # Add Password Button
